3ffe1a8fd84064b73d7fb4a8660b0564a829b623
Luke Li <leezhc@gmail.com>
2017-04-19 23:55:02 -0700

    delete modal added

--- src/components/frontViewComponent/table/DeletBtnComponent.js
+++ src/components/frontViewComponent/table/DeletBtnComponent.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import {Modal, Button} from 'react-bootstrap';
 
 require('styles/frontViewComponent/table/DeletBtn.css');
 
@@ -8,16 +9,40 @@ class DeletBtnComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      showModal : false
     }
   }
   delete(){
     this.props.deleteHandle(this.props.item);
+    this.close()
+  }
+  close() {
+    this.setState({ showModal: false });
+  }
+  open() {
+    this.setState({ showModal: true });
   }
   render() {
+    let modalEl = <Modal show={this.state.showModal} onHide={this.close.bind(this)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Delete</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Please confirm delete
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="danger" onClick={this.delete.bind(this)}>Delete</Button>
+            <Button onClick={this.close.bind(this)}>Cancel</Button>
+          </Modal.Footer>
+        </Modal>;
     return (
-      <button onClick={()=>this.delete()} className="deletbtn-component btn btn-default">
-        <i className="fa fa-trash " aria-hidden="true " />
-      </button>
+      <div className="deletbtn-component">
+        <button onClick={()=>this.open()} className="btn btn-default">
+          <i className="fa fa-trash " aria-hidden="true " />
+        </button>
+        {this.state.showModal?modalEl:null}
+      </div>
+      
     );
   }
 }
